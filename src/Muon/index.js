39e886b337804f6a7c044777f1a25fe79bb888af
maikCyphlock <maikolaguilar656@gmail.com>
2024-03-04 20:27:32 -0400

    ✨ feat: Fiber to handler virtual dom tasks

--- src/Muon/index.js
+++ src/Muon/index.js
@@ -27,32 +27,82 @@ function createTextElement(text) {
  *  @param {object} element
  *  @param {HTMLElement} container 
  */
-function render(element, container) {
-    const dom = element.type === "TEXT_ELEMENT" ? document.createTextNode(element.props.nodeValue) : document.createElement(element.type);
+function createDom(fiber) {
+    const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode(fiber.props.nodeValue) : document.createElement(fiber.type);
 
     const isProperty = key => key !== "children"
-    Object.keys(element.props)
+    Object.keys(fiber.props)
         .filter(isProperty)
         .forEach(name => {
-            dom[name] = element.props[name]
+            dom[name] = fiber.props[name]
         })
 
-
-    element.props.children.forEach(child =>
-        render(child, dom)
-    )
-    container.appendChild(dom);
+    return dom
 }
 
+function render(element, container) {
+    //TODO: implement render
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element]
+        }
+    }
+}
 let nextUnitOfWork = null
 
 function workLoop(deadline) {
     let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        shouldYield = deadline.timeRemaining() < 1
+    }
     requestIdleCallback(workLoop)
 }
 requestIdleCallback(workLoop)
 
-const element = (
+function performUnitOfWork(fiber) {
+    if (!fiber.dom) {
+        fiber.dom = createDom(fiber)
+    }
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom)
+    }
+
+    const elements = fiber.props.children;
+    let index = 0;
+    let prevSibling = null;
+    while (index < elements.length) {
+        const element = elements[index];
+        const newFiber = {
+            type: element.type,
+            props: element.props,
+            parent: fiber,
+            dom: null,
+        }
+        if (index === 0) {
+            fiber.child = newFiber
+        } else {
+            prevSibling.sibling = newFiber
+        }
+        prevSibling = newFiber
+        index++
+    }
+
+    if (fiber.child) {
+        return fiber.child
+    }
+    let newFiber = fiber
+    while (nextFiber) {
+        if (nextFiber.sibling) {
+            return nextFiber.sibling
+        }
+        nextFiber = nextFiber.parent
+    }
+}
+const fiber = (
     <div id="foo">
         <a>bar</a>
         <b />
@@ -64,5 +114,5 @@ const element = (
     </div>
 )
 
-render(element, document.getElementById('root'))
-console.log(element)
+render(fiber, document.getElementById('root'))
+console.log(fiber)
