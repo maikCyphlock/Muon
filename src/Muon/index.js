ca0559270fc1d607db1c1eeb3a5ce68b5472a104
maikCyphlock <maikolaguilar656@gmail.com>
2024-03-05 20:19:19 -0400

    ✨ feat: add my first hook, useState, with fully support

--- src/Muon/index.js
+++ src/Muon/index.js
@@ -28,21 +28,15 @@ function createTextElement(text) {
 function createDom(fiber) {
     const dom = fiber.type === "TEXT_ELEMENT" ? document.createTextNode(fiber.props.nodeValue) : document.createElement(fiber.type);
 
-    const isProperty = key => key !== "children"
-    Object.keys(fiber.props)
-        .filter(isProperty)
-        .forEach(name => {
-            dom[name] = fiber.props[name]
-        })
+    updateDom(dom, {}, fiber.props)
 
     return dom
 }
 const isEvent = key => key.startsWith("on")
 const isProperty = key =>
     key !== "children" && !isEvent(key)
-
-
-const isNew = (prev, next) => key => prev[key] !== next[key]
+const isNew = (prev, next) => key =>
+    prev[key] !== next[key]
 const isGone = (prev, next) => key => !(key in next)
 function updateDom(dom, prevProps, nextProps) {
     //Remove old or changed event listeners
@@ -186,9 +180,44 @@ function performUnitOfWork(fiber) {
         nextFiber = nextFiber.parent
     }
 }
+
+let wipFiber = null
+let hookIndex = null
 function updateFunctionComponent(fiber) {
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
     const children = [fiber.type(fiber.props)]
     reconcileChildren(fiber, children)
+}
+function useState(initial) {
+    const oldHook =
+        wipFiber.alternate &&
+        wipFiber.alternate.hooks &&
+        wipFiber.alternate.hooks[hookIndex]
+
+    const hook = {
+        state: oldHook ? oldHook.state : initial,
+        queue: []
+    }
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+        hook.state = action(hook.state)
+    })
+    const setState = action => {
+
+        hook.queue.push(action)
+        progressRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot
+        }
+        nextUnitOfWork = progressRoot
+        deletions = []
+    }
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state, setState]
 }
 
 function updateHostComponent(fiber) {
@@ -251,14 +280,27 @@ function reconcileChildren(progressFiber, elements) {
 const Muon = {
     createElement,
     render,
+    useState,
 }
 
 
 
 /** @jsx Muon.createElement */
-function App(props) {
-    return <h1>Hi {props.name}</h1>
+function Counter() {
+    const [state, setState] = Muon.useState(1)
+    const handlerClick = () => setState(c => c + 1)
+    return (
+
+        <div>
+            <button onClick={handlerClick}>
+                Count: {state}
+            </button>
+            <div onClick={() => setState(c => c - 1)}>decrement</div>
+        </div>
+
+
+    )
 }
-const element = <App name="foo" />
+const element = <Counter />
 const container = document.getElementById("root")
 Muon.render(element, container)
